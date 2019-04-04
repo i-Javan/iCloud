e18d8bee5e2bb9d4d1a47652989758376aaafc02
Javan <18037916522@qq.com>
2019-04-04 14:23:57 +0800

    add by javan 提交配置文件测试使用 20190444

--- iCloud/iCloud-ConfigClient/src/main/java/com/iCloud/ConfigClientApplication.java
+++ iCloud/iCloud-ConfigClient/src/main/java/com/iCloud/ConfigClientApplication.java
@@ -3,11 +3,13 @@ package com.iCloud;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RefreshScope
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
