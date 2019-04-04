7280663298996b75c61c5c5452af912b716ef82c
Javan <18037916522@qq.com>
2019-04-04 15:40:29 +0800

    add by javan 提交Config纳入Eureka管理 20190444

--- iCloud/iCloud-ConfigServer-Colony/src/main/java/com/iCloud/ConfigServerColonyApplication.java
+++ iCloud/iCloud-ConfigServer-Colony/src/main/java/com/iCloud/ConfigServerColonyApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServerApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
-    }
+public class ConfigServerColonyApplication {
 
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerColonyApplication.class, args);
+    }
 }
