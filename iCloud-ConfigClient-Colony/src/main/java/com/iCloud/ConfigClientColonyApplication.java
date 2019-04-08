1ccbc71e19444feab5b86e6da75da2323f531293
Javan <18037916522@qq.com>
2019-04-08 14:14:53 +0800

    add by javan 提交代码 20190448

--- iCloud-ConfigClient-Colony/src/main/java/com/iCloud/ConfigClientColonyApplication.java
+++ iCloud-ConfigClient-Colony/src/main/java/com/iCloud/ConfigClientColonyApplication.java
@@ -3,13 +3,17 @@ package com.iCloud;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @RefreshScope
+//@EnableEurekaClient
+//@EnableDiscoveryClient
 public class ConfigClientColonyApplication {
 
     public static void main(String[] args) {
